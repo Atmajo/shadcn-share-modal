3fd73c1558e2d487b51bbcfc6e5c0f11ceec6ac1
Atmajo <atmajoc@gmail.com>
2024-07-05 00:19:38 +0530

    checking

--- app/page.tsx
+++ app/page.tsx
@@ -4,64 +4,6 @@ import ShareComp from "@/components/share-component";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useState } from "react";
-import { CodeBlock, dracula } from "react-code-blocks";
-
-const ShareCode = `export const ShareComp = ({ link }: ShareProps) => {
-    new ClipboardJS(".copy");
-    
-    return (
-      <div className="border border-gray-500 rounded-lg w-96 ">
-        <div className="flex justify-between items-center border-b border-gray-500 px-4 py-2">
-          <h1 className="text-lg font-semibold">Share</h1>
-          <Share2 className="w-5 h-5" />
-        </div>
-        <div className="px-4 py-6 space-y-4">
-          <div className="flex items-center space-x-2">
-            <div className="grid flex-1 gap-2">
-              <Label htmlFor="link" className="sr-only">
-                Link
-              </Label>
-              <Input
-                id="link"
-                defaultValue={link}
-                readOnly
-                className="text-text"
-              />
-            </div>
-            <Button
-              type="submit"
-              size="sm"
-              className="p-4 copy"
-              data-clipboard-action="copy"
-              data-clipboard-target="#link"
-            >
-              <span className="sr-only">Copy</span>
-              <CopyIcon className="h-4 w-4" />
-            </Button>
-          </div>
-          <div className="flex space-x-2">
-            <FacebookShare url={link!} size={48} borderRadius={10} />
-            <WhatsappShare url={link!} size={48} borderRadius={10} />
-            <TwitterShare url={link!} size={48} borderRadius={10} />
-            <EmailShare url={link!} size={48} borderRadius={10} />
-          </div>
-        </div>
-      </div>
-    );
-  };`;
-
-const ImportCode = `import { CopyIcon, Share2 } from "lucide-react";
-import React from "react";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import {
-  EmailShare,
-  FacebookShare,
-  TwitterShare,
-  WhatsappShare,
-} from "react-share-kit";
-import ClipboardJS from "clipboard";`;
 
 export default function Home() {
   const [code, setCode] = useState(false);
