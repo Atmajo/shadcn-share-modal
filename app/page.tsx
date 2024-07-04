d5ecfcc72f4d6a69caaaf324acb9c2bf9fdc73db
Atmajo <atmajoc@gmail.com>
2024-07-05 00:28:43 +0530

    cholbe monehocche

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,64 @@ import ShareComp from "@/components/share-component";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useState } from "react";
+import { CodeBlock, dracula } from "react-code-blocks";
+
+const ShareCode = `export const ShareComp = ({ link }: ShareProps) => {
+  new ClipboardJS(".copy");
+  
+  return (
+    <div className="border border-gray-500 rounded-lg w-96 ">
+      <div className="flex justify-between items-center border-b border-gray-500 px-4 py-2">
+        <h1 className="text-lg font-semibold">Share</h1>
+        <Share2 className="w-5 h-5" />
+      </div>
+      <div className="px-4 py-6 space-y-4">
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input
+              id="link"
+              defaultValue={link}
+              readOnly
+              className="text-text"
+            />
+          </div>
+          <Button
+            type="submit"
+            size="sm"
+            className="p-4 copy"
+            data-clipboard-action="copy"
+            data-clipboard-target="#link"
+          >
+            <span className="sr-only">Copy</span>
+            <CopyIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        <div className="flex space-x-2">
+          <FacebookShare url={link!} size={48} borderRadius={10} />
+          <WhatsappShare url={link!} size={48} borderRadius={10} />
+          <TwitterShare url={link!} size={48} borderRadius={10} />
+          <EmailShare url={link!} size={48} borderRadius={10} />
+        </div>
+      </div>
+    </div>
+  );
+};`;
+
+const ImportCode = `import { CopyIcon, Share2 } from "lucide-react";
+import React from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import {
+EmailShare,
+FacebookShare,
+TwitterShare,
+WhatsappShare,
+} from "react-share-kit";
+import ClipboardJS from "clipboard";`;
 
 export default function Home() {
   const [code, setCode] = useState(false);
@@ -25,7 +83,39 @@ export default function Home() {
         </div>
       </div>
       <div className="mt-14">
-        <ShareComp link="https://atmajo.tech" />
+        {!code && <ShareComp link="https://atmajo.tech" />}
+        {code && (
+          <div className="flex flex-col justify-center gap-y-5">
+            <pre
+              className="mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900"
+              data-language="bash"
+              data-theme="default"
+            >
+              <code
+                className="relative flex justify-between rounded bg-muted px-2 py-[0.2rem] font-mono text-sm"
+                data-language="bash"
+                data-theme="default"
+              >
+                <input
+                  className="line bg-transparent text-white w-full"
+                  defaultValue="npm i react-share-kit clipboard"
+                />
+              </code>
+            </pre>
+            <CodeBlock
+              text={ImportCode}
+              language="tsx"
+              showLineNumbers={true}
+              theme={dracula}
+            />
+            <CodeBlock
+              text={ShareCode}
+              language="tsx"
+              showLineNumbers={true}
+              theme={dracula}
+            />
+          </div>
+        )}
       </div>
       <div className="relative flex flex-col justify-center items-end max-w-2xl">
         <p className="text-sm text-center opacity-80 font-normal mt-32">
